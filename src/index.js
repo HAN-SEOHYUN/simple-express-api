935bdb6b93a4e27c6eb8e2ad05d0413f1bb9bf73
sian-han <sian.han@dgmit.com>
2022-12-08 18:54:42 +0900

    study : express rest api

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
 const express = require("express");
-const groceriesRoute = require('./routes/groceries');
-const marketsRoute = require('./routes/markets');
-
-const app = express(); //give instance of application
+const bodyParser = require('body-parser');
 const PORT = 3001;
+const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use((req,res,next)=>{
-    console.log(`${req.method}:${req.url}`);
-    next(); 
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
-// app.use(groceriesRoute);
-app.use('/api/v1/groceries',groceriesRoute); //prefix the api
-app.use('/api/v1/markets',marketsRoute); 
+const groceryRouter = require('./routes/grocery');
+app.use('/grocery',groceryRouter);
+
+
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
-
-/*app.get(
-  "/groceries",
-  (req, res, next) => {
-    console.log("Before Handling Request");
-    next();
-  },
-  (req, res, next) => {
-    res.send(groceryList);
-    next();
-  },
-  (req,res,next) => {
-    console.log('Finish Executing Get request');
-    next();  
-  },
-  (req, res, next) => {
-    res.send(403);
-  }
-);*/
-
-/*app.get("/groceries/:item",(req,res)=>{
-    console.log(req.params.item);
-    res.send(200); 
-});*/
-
-
-
