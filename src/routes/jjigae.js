61b2cb4666a0e60168dbb4d753eba3a248d5785d
sian-han <sian.han@dgmit.com>
2022-12-09 18:49:38 +0900

    study : express rest api

--- src/routes/jjigae.js
+++ src/routes/jjigae.js
@@ -1,53 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const db = require('../db');
+const db = require("../db");
 
-router.get('',(req,res,next)=>{
-    db.getAllJjigae((rows)=>{
-        res.send(rows); 
-    });
+router.get("", (req, res, next) => {
+  db.getAllJjigae((rows) => {
+    res.send(rows);
+  });
 });
 
-router.get('/:id',(req,res)=>{
-    const key = 'id';
+router.get("/:id", (req, res) => {
+  const key = "id";
 
-    db.getJjigaeByKey(key, req.params.id, (row)=>{
-        res.send(row);
-    });
+  db.getJjigaeByKey(key, req.params.id, (row) => {
+    res.send(row);
+  });
 });
 
-router.post('/', (req,res)=>{
-    const param = req.body;
-    db.insertJjigae(param, ()=>{
-        res.redirect('/jjigae');
-    });
+router.post("/", (req, res) => {
+  const key = "name";
+  const value = "'" + req.body.name + "'";
+
+  db.insertJjigae(req.body, () => {
+    res.redirect("/jjigae");
+  });
 });
 
-router.delete('/:id',(req,res)=>{
-    const id = req.params.id;
-    db.deleteJjigae(id, ()=>{
-        res.redirect('/jjigae');
-    });
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db.deleteJjigae(id, () => {
+    res.redirect("/jjigae");
+  });
 });
 
-router.put('/', (req,res) => {
-    const key = 'name';
-    const value = "'"+ req.body.name + "'";
+router.put("/", (req, res) => {
+  const key = "name";
+  const value = "'" + req.body.name + "'";
 
-    let detailResponse;
-    db.getJjigaeByKey( key,  value, (row)=>{
-        detailResponse = row;
-        console.log('data : ' , detailResponse);
-    });
+  let detailResponse;
+  db.getJjigaeByKey(key, value, (row) => {
+    detailResponse = row;
+  });
 
-    if (detailResponse == "") {
-        console.log("없음");
-    }
+  if(!detailResponse){
+    res.send('새로운 객체는 post 요청 ! ');
+  }
 
-    db.updateJjigaeByName(req.body, ()=>{
-        res.redirect('/jjigae');    
-    });
+  db.updateJjigaeByName(req.body, () => {
+    res.redirect("/jjigae");
+  });
 });
 
 module.exports = router;
